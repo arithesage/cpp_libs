476781c16208b8acecb12806b1796a41a7dbdf26
arithesage <ariaththesage@gmail.com>
2025-02-19 12:22:08 +0100

    Using regular function pointers isn't very complicated, but surely this will help with the syntax.

--- FunctionPointers.hpp
+++ FunctionPointers.hpp
@@ -2,6 +2,175 @@
 #define __FUNCTION_POINTERS__
 
 
+//////////////////////////////////////////////////////////////////////////
+/// POINTERS TO FUNCTIONS
+//////////////////////////////////////////////////////////////////////////
+
+/**
+ * Stores a pointer to a function that returns void
+ * and takes no parameters
+ */
+using FuncPtr = void (*) ();
+
+
+/**
+ * Stores a pointer to a function that returns a value of type R
+ * and takes no parameters
+ */
+template <typename R>
+using FuncPtrR = R (*) ();
+
+
+/**
+ * Stores a pointer to a function that returns void
+ * and takes one parameter
+ */
+template <typename P1>
+using FuncPtrP1 = void (*) (P1 p1);
+
+
+/**
+ * Stores a pointer to a function that returns a value of type R
+ * and takes one parameter
+ */
+template <typename R, typename P1>
+using FuncPtrRP1 = R (*) (P1 p1);
+
+
+/**
+ * Stores a pointer to a function that void
+ * and takes two parameters.
+ */
+template <typename P1, typename P2>
+using FuncPtrP2 = void (*) (P1 p1, P2 p2);
+
+
+/**
+ * Stores a pointer to a function that returns a value of type R
+ * and takes two parameters.
+ */
+template <typename R, typename P1, typename P2>
+using FuncPtrRP2 = R (*) (P1 p1, P2 p2);
+
+
+/**
+ * Stores a pointer to a function that returns void
+ * and takes three parameters.
+ */
+template <typename P1, typename P2, typename P3>
+using FuncPtrP3 = void (*) (P1 p1, P2 p2, P3 p3);
+
+
+/**
+ * Stores a pointer to a function that returns a value of type R
+ * and takes three parameters.
+ */
+template <typename R, typename P1, typename P2, typename P3>
+using FuncPtrRP3 = R (*) (P1 p1, P2 p2, P3 p3);
+
+
+/**
+ * Stores a pointer to a function that returns void
+ * and takes four parameters.
+ */
+template <typename P1, typename P2, typename P3, typename P4>
+using FuncPtrP4 = void (*) (P1 p1, P2 p2, P3 p3, P4 p4);
+
+
+/**
+ * Stores a pointer to a function that returns a value of type R
+ * and takes four parameters.
+ */
+template <typename R, typename P1, typename P2, typename P3, typename P4>
+using FuncPtrRP4 = R (*) (P1 p1, P2 p2, P3 p3, P4 p4);
+
+
+/**
+ * Stores a pointer to a function that returns void
+ * and takes five parameters.
+ */
+template <typename P1, typename P2, typename P3, typename P4,
+          typename P5>
+using FuncPtrP5 = void (*) (P1 p1, P2 p2, P3 p3, P4 p4,
+                            P5 p5);
+
+
+/**
+ * Stores a pointer to a function that returns a value of type R
+ * and takes five parameters.
+ */
+template <typename R, typename P1, typename P2, typename P3, typename P4,
+                      typename P5>
+using FuncPtrRP5 = R (*) (P1 p1, P2 p2, P3 p3, P4 p4,
+                          P5 p5);
+
+
+/**
+ * Stores a pointer to a function that returns void
+ * and takes six parameters.
+ */
+template <typename P1, typename P2, typename P3, typename P4,
+          typename P5, typename P6>
+using FuncPtrP6 = void (*) (P1 p1, P2 p2, P3 p3, P4 p4,
+                            P5 p5, P6 p6);
+
+
+/**
+ * Stores a pointer to a function that returns a value of type R
+ * and takes six parameters.
+ */
+template <typename R, typename P1, typename P2, typename P3, typename P4,
+                      typename P5, typename P6>
+using FuncPtrRP6 = R (*) (P1 p1, P2 p2, P3 p3, P4 p4,
+                          P5 p5, P6 p6);
+
+
+/**
+ * Stores a pointer to a function that returns void
+ * and takes seven parameters.
+ */
+template <typename P1, typename P2, typename P3, typename P4,
+          typename P5, typename P6, typename P7>
+using FuncPtrP7 = void (*) (P1 p1, P2 p2, P3 p3, P4 p4,
+                            P5 p5, P6 p6, P7 p7);
+
+
+/**
+ * Stores a pointer to a function that returns a value of type R
+ * and takes seven parameters.
+ */
+template <typename R, typename P1, typename P2, typename P3, typename P4,
+                      typename P5, typename P6, typename P7>
+using FuncPtrRP7 = R (*) (P1 p1, P2 p2, P3 p3, P4 p4,
+                          P5 p5, P6 p6, P7 p7);
+
+
+/**
+ * Stores a pointer to a function that returns void
+ * and takes eight parameters.
+ */
+template <typename P1, typename P2, typename P3, typename P4,
+          typename P5, typename P6, typename P7, typename P8>
+using FuncPtrP8 = void (*) (P1 p1, P2 p2, P3 p3, P4 p4,
+                            P5 p5, P6 p6, P7 p7, P8 p8);
+
+
+/**
+ * Stores a pointer to a function that returns a value of type R
+ * and takes eight parameters.
+ */
+template <typename R, typename P1, typename P2, typename P3, typename P4,
+                      typename P5, typename P6, typename P7, typename P8>
+using FuncPtrRP8 = R (*) (P1 p1, P2 p2, P3 p3, P4 p4,
+                          P5 p5, P6 p6, P7 p7, P8 p8);
+
+
+
+
+//////////////////////////////////////////////////////////////////////////
+/// POINTERS TO OBJECTS METHODS
+//////////////////////////////////////////////////////////////////////////
+
 /**
  * Stores a pointer to a method belonging to class C.
  * To refer to a class method, use format &ClassName:MethodName.
