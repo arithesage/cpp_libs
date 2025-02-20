b8a65fb99c36535ce8a4953a7676ce7612357261
arithesage <ariaththesage@gmail.com>
2025-02-20 10:51:51 +0100

    Working on event system. Made 4 event-listener pairs to provide up to four parameters and one without them. Can be easily expanded if needed as with the function pointers.

--- Listener.hpp
+++ Listener.hpp
@@ -1,16 +1,53 @@
 #ifndef __EVENT_LISTENER__
 #define __EVENT_LISTENER__
 
+#include <FunctionPointers.hpp>
+
+
 class Listener
 {
+    private:
+        //MethodPtr onNotify = nullptr;
+
     public:
-        void Notify ();
+        void Notify () {}
+        void OnNotify ()
+        {
+
+        }
 };
 
 
-void Listener::Notify ()
+template <typename P1>
+class Listener1P
 {
-    
-}
+    public:
+        void Notify (P1 p1) {}
+};
+
+
+template <typename P1, typename P2>
+class Listener2P
+{
+    public:
+        void Notify (P1 p1, P2 p2) {}
+};
+
+
+template <typename P1, typename P2, typename P3>
+class Listener3P
+{
+    public:
+        void Notify (P1 p1, P2 p2, P3 p3) {}
+};
+
+
+template <typename P1, typename P2, typename P3, typename P4>
+class Listener4P
+{
+    public:
+        void Notify (P1 p1, P2 p2, P3 p3, P4 p4) {}
+};
+
 
 #endif
